e6d38ab38118f0d644ea643fa3c798895b6f1373
JoseRRC <jbacalaoescarraga@aceproject.space>
2023-03-05 11:10:50 -0600

    Query Data Whit Static Generation (SSG)

--- pages/homes/[id].js
+++ pages/homes/[id].js
@@ -1,8 +1,11 @@
 // pages/homes/[id].js
-// 4.3 continue
 
-import Image from 'next/image';
-import Layout from '@/components/Layout';
+import Image from "next/image";
+import Layout from "@/components/Layout";
+import { PrismaClient } from "@prisma/client";
+
+//Instantiate Prisma Client
+const prisma = new PrismaClient();
 
 const ListedHome = (home = null) => {
   return (
@@ -11,7 +14,7 @@ const ListedHome = (home = null) => {
         <div className="flex flex-col sm:flex-row sm:justify-between sm:space-x-4 space-y-4">
           <div>
             <h1 className="text-2xl font-semibold truncate">
-              {home?.title ?? ''}
+              {home?.title ?? ""}
             </h1>
             <ol className="inline-flex items-center space-x-1 text-gray-500">
               <li>
@@ -40,10 +43,45 @@ const ListedHome = (home = null) => {
           ) : null}
         </div>
 
-        <p className="mt-8 text-lg">{home?.description ?? ''}</p>
+        <p className="mt-8 text-lg">{home?.description ?? ""}</p>
       </div>
     </Layout>
   );
 };
+
+export async function getStaticPaths() {
+  //Get all the homes IDs from the database
+  const homes = await prisma.home.findMany({
+    select: { id: true },
+  });
+
+  return {
+    paths: homes.map((home) => ({
+      params: { id: home.id },
+    })),
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  //Get the current home from the database
+
+  const home = await prisma.home.findUnique({
+    where: { id: params.id },
+  });
+
+  if (home) {
+    return {
+      props: JSON.parse(JSON.stringify(home)),
+    };
+  }
+
+  return {
+    redirect: {
+      destination: "/",
+      permanent: false,
+    },
+  };
+}
 
 export default ListedHome;
